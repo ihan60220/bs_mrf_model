a7cde8ca535cd47c775ee68aefffa8a00ebf7b20
ihan60220 <indonghan1308@gmail.com>
2025-02-27 22:01:27 -0600

    gitignore

--- mpes_reconstruction.py
+++ mpes_reconstruction.py
@@ -17,18 +17,18 @@ I = data['V']
 # Create MRF model
 print("creating mrf model...")
 mrf = MrfRec(E=E, kx=kx, ky=ky, I=I, eta=.12)
-print("is normalized:", mrf.I_normalized)
 mrf.I_normalized = True
 
 # Initialize mrf model with band structure approximation from DFT
 print("initializing E_0 to DFT calculations...")
-path_dft = './data/theory/WSe2_PBEsol_bands.mat'
+path_dft = './data/theory/WSe2_HSE06_bands.mat'
 
-band_index = 4
+band_index = 0  # there is a total of 80 different bands
 offset = 0.6    # default was -0.1
 k_scale = 1.1
 
 kx_dft, ky_dft, E_dft = mrf.loadBandsMat(path_dft)
+print("band structure shape:", E_dft.shape)
 mrf.initializeBand(kx=kx_dft, ky=ky_dft, Eb=E_dft[band_index,...], offset=offset, kScale=k_scale, flipKAxes=True)
 
 # Plot slices with initialiation to check offset and scale
@@ -40,11 +40,12 @@ plt.savefig("./results/init_kx_0")
 
 # Run optimization to perform reconstruction
 print("training model...")
-eta = 0.3 # default 0.1
+eta = 0.1 # default 0.1
 n_epochs = 150 # default 150
 
 mrf.eta = eta
 mrf.iter_para(n_epochs)
+#mrf.iter_seq(n_epochs)  # doing sequentially
 
 # Plot results
 print("plotting reconstructed bands...")
