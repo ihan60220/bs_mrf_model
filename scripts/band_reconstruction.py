f212cc80b4e3a2967bf94bbcb8e1bb810f9895dd
ihan60220 <indonghan1308@gmail.com>
2025-02-28 21:24:21 -0600

    global commit

--- scripts/band_reconstruction.py
+++ scripts/band_reconstruction.py
@@ -10,76 +10,9 @@ from fuller.mrfRec import MrfRec
 from fuller.utils import loadHDF
 
 
-# preprocessing steps for graphing along high symmetry points
-
-# mpl.rcParams['font.family'] = 'sans-serif'
-# mpl.rcParams['font.sans-serif'] = 'Arial'
-mpl.rcParams['axes.linewidth'] = 2
-mpl.rcParams['pdf.fonttype'] = 42
-mpl.rcParams['ps.fonttype'] = 42
-
-# high symmetry points need to be defined for graphic Brioullin zone
-
-fdata = fp.readBinnedhdf5('./data/pes/1_sym.h5')
-mc = aly.MomentumCorrector(fdata['V'])
-
-mc.selectSlice2D(selector=slice(30, 32), axis=2)
-mc.featureExtract(mc.slice, method='daofind', sigma=6, fwhm=20, symscores=False)
-
-# False detection filter, if needed
-try:
-    mc.pouter_ord = mc.pouter_ord[[0,1,3,5,6,9],:]
-except:
-    pass
-
-# Define high-symmetry points
-G = mc.pcent # Gamma point
-K = mc.pouter_ord[0,:] # K point
-K1 = mc.pouter_ord[1,:] # K' point
-M = (K + K1) / 2 # M point
-
-# Define cutting path
-pathPoints = np.asarray([G, M, K, G])
-nGM, nMK, nKG = 70, 39, 79
-segPoints = [nGM, nMK, nKG]
-rowInds, colInds, pathInds = aly.points2path(pathPoints[:,0], pathPoints[:,1], npoints=segPoints)
-nSegPoints = len(rowInds)
-
-# plotting function for high symmetry points
-def plot_path(mrf, vmax, save_path, fname):
-    # Normalize data
-    imNorm = mrf.I / mrf.I.max()
-
-    # Sample the data along high-symmetry lines (k-path) connecting the corresponding high-symmetry points
-    pathDiagram = aly.bandpath_map(imNorm, pathr=rowInds, pathc=colInds, eaxis=2)
-
-    Evals = mrf.E
-    ehi, elo = Evals[0], Evals[449]
-
-    f, ax = plt.subplots(figsize=(10, 6))
-    plt.imshow(pathDiagram[:450, :], cmap='Blues', aspect=10.9, extent=[0, nSegPoints, elo, ehi], vmin=0, vmax=vmax)
-    ax.set_xticks(pathInds)
-    ax.set_xticklabels(['$\overline{\Gamma}$', '$\overline{\mathrm{M}}$',
-                        '$\overline{\mathrm{K}}$', '$\overline{\Gamma}$'], fontsize=15)
-    for p in pathInds[:-1]:
-        ax.axvline(x=p, c='r', ls='--', lw=2, dashes=[4, 2])
-    # ax.axhline(y=0, ls='--', color='r', lw=2)
-    ax.yaxis.set_major_locator(MultipleLocator(2))
-    ax.yaxis.set_minor_locator(MultipleLocator(1))
-    ax.yaxis.set_label_position("right")
-    ax.yaxis.tick_right()
-    ax.set_ylabel('Energy (eV)', fontsize=15, rotation=-90, labelpad=20)
-    ax.tick_params(axis='x', length=0, pad=6)
-    ax.tick_params(which='both', axis='y', length=8, width=2, labelsize=15)
-
-    full_path = save_path + fname
-    
-    plt.savefig(full_path, dpi=200)
-
-
 # Load preprocessed data
 print("loading preprocessed data...")
-data = loadHDF('./data/pes/3_smooth.h5')
+data = loadHDF('../data/pes/3_smooth.h5')
 E = data['E']
 kx = data['kx']
 ky = data['ky']
@@ -90,51 +23,62 @@ print("creating mrf model...")
 mrf = MrfRec(E=E, kx=kx, ky=ky, I=I, eta=.12)
 mrf.I_normalized = True
 
-# debugging
-
-print(E.size, kx.size, ky.size)
-
 # Initialize mrf model with band structure approximation from DFT
 print("initializing E_0 to DFT calculations...")
-path_dft = './data/theory/WSe2_HSE06_bands.mat'
+path_dft = '../data/theory/WSe2_HSE06_bands.mat'
 
-band_index = 15  # there is a total of 80 different bands
-offset = 0.4    # default was -0.1
-k_scale = 1.0
 
-kx_dft, ky_dft, E_dft = mrf.loadBandsMat(path_dft)
-print("band structure shape:", E_dft.shape)
+def reconstruct(band_index):
+    """
+    Wrapper function for reconstructing band structure given some parameters
+    """
 
-# possible modify source to train multiple bands at once
-mrf.initializeBand(kx=kx_dft, ky=ky_dft, Eb=E_dft[band_index,...], offset=offset, kScale=k_scale, flipKAxes=True)
+    band_index = 1  # there is a total of 80 different bands
+    offset = 0.4    # default was -0.1
+    k_scale = 1.0
 
-# Plot slices with initialiation to check offset and scale
-print("plotting initialized mrf...")
-mrf.plotI(ky=0, plotBandInit=True, cmapName='YlGn', bandColor='tab:orange', initColor='m')
-plt.savefig("./results/reconstruction/init_ky_0")
-mrf.plotI(kx=0, plotBandInit=True, cmapName='YlGn', bandColor='tab:orange', initColor='m')
-plt.savefig("./results/reconstruction/init_kx_0")
+    kx_dft, ky_dft, E_dft = mrf.loadBandsMat(path_dft)
+    print("band structure shape:", E_dft.shape)
 
-# Run optimization to perform reconstruction
-print("training model...")
-eta = 0.1 # default 0.1
-n_epochs = 150 # default 150
+    # possible modify source to train multiple bands at once
+    mrf.initializeBand(kx=kx_dft, ky=ky_dft, Eb=E_dft[band_index,...], offset=offset, kScale=k_scale, flipKAxes=True)
 
-mrf.eta = eta
-mrf.iter_para(n_epochs)
-#mrf.iter_seq(n_epochs)  # doing sequentially
+    # Plot slices with initialiation to check offset and scale
+    print("plotting initialized mrf...")
 
-# Plot results
-print("plotting reconstructed bands...")
-mrf.plotBands(surfPlot=True)
-plt.savefig("./results/reconstruction/bs_surface")
-mrf.plotI(ky=0, plotBand=True, plotBandInit=True, cmapName='YlGn', bandColor='tab:orange', initColor='m')
-plt.savefig("./results/reconstruction/band_ky_0")
-mrf.plotI(kx=0, plotBand=True, plotBandInit=True, cmapName='YlGn', bandColor='tab:orange', initColor='m')
-plt.savefig("./results/reconstruction/band_kx_0")
+    mrf.plotI(ky=0, plotBandInit=True, cmapName='YlGn', bandColor='tab:orange', initColor='m')
+    plt.savefig("../results/reconstruction/init_ky_0")
 
-# Save results
-path_save = 'results/reconstruction/'
-mrf.saveBand(f"{path_save}mrf_rec_{band_index}.h5", index=band_index)
+    mrf.plotI(kx=0, plotBandInit=True, cmapName='YlGn', bandColor='tab:orange', initColor='m')
+    plt.savefig("../results/reconstruction/init_kx_0")
 
-# think about maybe serializing the mrf for later use
+    # Run optimization to perform reconstruction
+    print("training model...")
+    eta = 0.1 # default 0.1
+    n_epochs = 150 # default 150
+
+    mrf.eta = eta
+    mrf.iter_para(n_epochs)
+
+    # Plot results
+    print("plotting reconstructed bands...")
+    mrf.plotBands(surfPlot=True)
+    plt.savefig("../results/reconstruction/bs_surface")
+
+    mrf.plotI(ky=0, plotBand=True, plotBandInit=True, cmapName='YlGn', bandColor='tab:orange', initColor='m')
+    plt.savefig("../results/reconstruction/band_ky_0")
+
+    mrf.plotI(kx=0, plotBand=True, plotBandInit=True, cmapName='YlGn', bandColor='tab:orange', initColor='m')
+    plt.savefig("../results/reconstruction/band_kx_0")
+
+    # Save results
+    path_save = '../results/band_data/'
+    mrf.saveBand(f"{path_save}mrf_rec_{band_index}.h5", index=band_index)
+
+    # think about maybe serializing the mrf for later use
+
+
+# call the wrapper function
+for band_index in range(14):
+    print(f"reconstructing the {band_index}th band...")
+    reconstruct(band_index)
--- scripts/visualize_dft_bands.py
+++ scripts/visualize_dft_bands.py
@@ -1,34 +1,52 @@
 
-from fuller.mrfRec import MrfRec
+import warnings as wn
+wn.filterwarnings("ignore")
 
-import matplotlib.pyplot as plt
+import os
 import numpy as np
+import fuller
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
+from mpes import fprocessing as fp, analysis as aly
+from scipy import io as sio
+from fuller.utils import loadHDF
 
-from matplotlib import cm
-from matplotlib.ticker import LinearLocator
+# Load preprocessed photoemission data
+bcsm = np.load(r'../data/processed/hslines/WSe2_vcut.npy')
+Evals = fp.readBinnedhdf5(r'../data/pes/3_smooth.h5')['E']
+ehi, elo = Evals[0], Evals[469]
 
+paths = np.load(r'../data/processed/hslines/WSe2_kpath.npz')
 
-# import dft band data
-path_dft = './data/theory/WSe2_HSE06_bands.mat'
+reconbands = {}
+for i in range(14):
+    bands = loadHDF('../results/reconstruction/mrf_rec_0.h5')
+    print(f"band {i}", bands[0])
+    bdi = aly.bandpath_map(np.moveaxis(bands, 0, 2), pathr=paths['rowInds'], pathc=paths['colInds'], eaxis=2)
+    reconbands[f"band_{i}"] = bdi.T
 
-kx, ky, E = MrfRec.loadBandsMat(path_dft)
+pos = paths['pathInds']
+pos[-1] -= 1
 
-band_index = 30  # there is a total of 80 different bands
-offset = 0.4    # default was -0.1
-k_scale = 1.0
+ff, axa = plt.subplots(1, 1, figsize=(10.5, 8))
+im = axa.imshow(bcsm, cmap='Blues', extent=[0, 185, elo, ehi], aspect=12)
+# axa.plot(dftbands['HSE'][:,:14] - hse_th_shift, 'r--', zorder=2)
+for ib in range(14):
+    axa.plot(reconbands['LDA'][:,ib] + 0.65, color='r', zorder=1)
+axa.tick_params(axis='y', length=8, width=2, labelsize=15)
+axa.tick_params(axis='x', length=0, labelsize=15, pad=8)
+axa.set_ylim([elo, ehi])
+axa.set_xticks(pos)
+axa.set_xticklabels(['$\overline{\Gamma}$', '$\overline{\mathrm{M}}$',
+                       '$\overline{\mathrm{K}}$', '$\overline{\Gamma}$'])
+axa.set_ylabel('Energy (eV)', fontsize=20)
+for p in pos[:-1]:
+        axa.axvline(x=p, c='k', ls='--', lw=2, dashes=[4, 2])
+        
+axa.set_title('Reconstruction', fontsize=15, x=0.8, y=0.9)
+cax = inset_axes(axa, width="3%", height="30%", bbox_to_anchor=(220, 90, 440, 200))
+cb = plt.colorbar(im, cax=cax, ticks=[])
+cb.ax.set_ylabel('Intensity', fontsize=15, rotation=-90, labelpad=17)
 
-
-# plotting
-
-fig = plt.figure()
-ax = fig.add_subplot(projection='3d')
-
-print(kx)
-print(ky)
-
-ax.scatter(kx, ky, E)
-ax.set_xlabel('X Label')
-ax.set_ylabel('Y Label')
-ax.set_zlabel('Z Label')
-
-plt.show()
+plt.savefig('../results/reconstruction/multiple_bands.png')
