1a1b3186784e71618a02e7b53f829d769b82fd9e
ihan60220 <indonghan1308@gmail.com>
2025-03-06 01:46:43 -0600

    hpc migration

--- scripts/bs_epoch_gif.py
+++ scripts/bs_epoch_gif.py
@@ -2,7 +2,7 @@ import imageio
 
 images = []
 
-for i in range(10, 110, 10):
-    images.append(imageio.imread(f"../results/reconstruction/bs_recon_{i}.png"))
+for i in range(10, 80, 10):
+    images.append(imageio.imread(f"results/reconstruction/reconstruction/bs_recon_{i}.png"))
 
-imageio.mimsave(f'../results/reconstruction/bs_recon.gif', images)
+imageio.mimsave(f'results/reconstruction/bs_recon_10_to_70.gif', images, fps=2)
