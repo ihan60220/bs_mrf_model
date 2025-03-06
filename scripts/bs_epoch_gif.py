f92a690da8f67a4b06b1bbe122c5416212b514bb
ihan60220 <indonghan1308@gmail.com>
2025-03-06 02:07:52 -0600

    full bs reconstruction

--- scripts/bs_epoch_gif.py
+++ scripts/bs_epoch_gif.py
@@ -2,7 +2,8 @@ import imageio
 
 images = []
 
-for i in range(10, 80, 10):
-    images.append(imageio.imread(f"results/reconstruction/reconstruction/bs_recon_{i}.png"))
+for i in range(10, 110, 10):
+    print(i)
+    images.append(imageio.imread(f"results/reconstruction/bs_recon_{i}.png"))
 
-imageio.mimsave(f'results/reconstruction/bs_recon_10_to_70.gif', images, fps=2)
+imageio.mimsave(f'results/reconstruction/bs_full_recon.gif', images)
